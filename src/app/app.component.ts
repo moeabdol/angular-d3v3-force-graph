7c9af6f6b07cbb91f2d4a08dc5a523c4841b6a68
moeabdol <mohd.a.saed@gmail.com>
2017-09-27 16:31:07 +0300

    Implement force directed graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import * as d3 from 'd3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
   ngOnInit() {
     console.log('d3.js version:', d3['version']);
+
+    this.forceGraph();
+  }
+
+  forceGraph() {
+    // const width = 960;
+    // const height = 500;
+    // const showNames = false;
+    // let root, nodes, links;
+    //
+    // const force = d3.layout.force()
+    //   .linkDistance(80)
+    //   .charge(-120)
+    //   .gravity(.05)
+    //   .size([width, height])
+    //   .on('tick', tick);
+    //
+    // const svg = d3.select('body').append('svg')
+    //   .attr('width', width)
+    //   .attr('height', height);
+    //
+    // let link = svg.selectAll('.link');
+    // let node = svg.selectAll('node');
+    //
+    // d3.json('assets/demo.json', (err, json) => {
+    //   root = json;
+    //   start();
+    // });
+    //
+    // function start() {
+    //   nodes = flatten(root);
+    //   links = d3.layout.tree().links(nodes);
+    //   for (let i = 0; i < nodes.length; i++) {
+    //     if (nodes[i].collapse) {
+    //       toggle(nodes[i]);
+    //     }
+    //   }
+    //   restart();
+    // }
+    //
+    // function restart() {
+    //   nodes = flatten(root);
+    //   links = d3.layout.tree().links(nodes);
+    //   update();
+    // }
+    //
+    // function update() {
+    //   force
+    //     .nodes(nodes)
+    //     .links(links)
+    //     .start();
+    //
+    //   link = link.data<any>(links, (d) => d.target.id);
+    //   link.exit().remove();
+    //   link.enter().insert('line', '.node')
+    //     .attr('class', 'link');
+    // }
+    //
+    // function tick() {
+    // }
+    //
+    // function flatten(r) {
+    // }
+    //
+    // function toggle(n) {
+    // }
   }
 }
